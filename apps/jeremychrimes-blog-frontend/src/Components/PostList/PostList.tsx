7d3a40f4d12d93740429a8738a4168c5f482e935
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2024-07-28 00:51:17 +1000

    Get from blog api

---
+++ apps/jeremychrimes-blog-frontend/src/Components/PostList/PostList.tsx
@@ -0,0 +1,45 @@
+import { List, Table } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface IWPPost {
+  slug: string;
+  id: number;
+  title: {
+    rendered: string;
+  };
+}
+export function PostList() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["postData"],
+    queryFn: () =>
+      axios.get<IWPPost[]>("https://jeremychrimes.com/wp-json/wp/v2/posts", {
+        responseType: "json",
+      }),
+  });
+
+  if (isPending) {
+    return "Loading";
+  }
+
+  if (error) {
+    return `An error has occured: {error}`;
+  }
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Slug</Table.Th>
+          <Table.Th>Title</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      {data.data.map((post) => (
+        <Table.Tr>
+          <Table.Td>{post.slug}</Table.Td>
+          <Table.Td>{post.title.rendered}</Table.Td>
+        </Table.Tr>
+      ))}
+    </Table>
+  );
+}
